5de3f97d18c4a83a653435ebf926f50e8d991585
Davy ROBERT <davyrobert90@gmail.com>
2022-11-20 22:16:56 +0100

    update methode PUT movies and users

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const usersHandlers = require('./usersHandlers');
 app.get('/api/users', usersHandlers.getUsers);
 app.get('/api/users/:id', usersHandlers.getUsersById);
 app.post('/api/users', usersHandlers.addUser);
+app.put('/api/users/:id', usersHandlers.updateUserById);
 
 /************************************************/
 
--- usersHandlers.js
+++ usersHandlers.js
@@ -61,7 +61,29 @@ const addUser = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error saving the movie');
+      res.status(500).send('Erreur de sauvegarde de l utilisateur');
+    });
+};
+
+const updateUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      'update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?',
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send('Not Found');
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Erreur d édition de l utilisateur');
     });
 };
 
@@ -69,4 +91,5 @@ module.exports = {
   getUsers,
   getUsersById,
   addUser,
+  updateUserById,
 };
